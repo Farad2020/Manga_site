d8687c817df40cb1524a50f9f8fb7485e3f488b3
Farad2020 <fnirc@mail.ru>
2020-01-07 00:26:05 +0600

    lil bit of moving forward

---
+++ AiManga/AiMangaApp/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from multiselectfield import MultiSelectField
+# Create your models here.
+genres = (
+    ("Action", "Action"), ("Adventure", "Adventure"), ("Comedy", "Comedy"), ("Cyberpunk", "Cyberpunk"),
+    ("Demons", "Demons"), ("Drama", "Drama"), ("Ecchi", "Ecchi"), ("Fantasy", "Fantasy"),
+    ("Game", "Game"), ("Harem", "Harem"), ("Hentai", "Hentai"), ("Historical", "Historical"),
+    ("Horror", "Horror"), ("Isekai", "Isekai"), ("Josei", "Josei"), ("Kids", "Kids"), ("Magic", "Magic"),
+    ("Martial Arts", "Martial Arts"), ("Mecha", "Mecha"), ("Military", "Military"), ("Music", "Music"),
+    ("Mystery", "Mystery"), ("Parody", "Parody"), ("Police", "Police"), ("Post-Apocalyptic", "Post-Apocalyptic"),
+    ("Psychological", "Psychological"), ("Reverse Harem", "Reverse Harem"), ("Romance", "Romance"),
+    ("School", "School"), ("Sci-Fi", "Sci-Fi"), ("Seinen", "Seinen"), ("Shoujo", "Shoujo"),
+    ("Shoujo-ai", "Shoujo-ai"), ("Shounen", "Shounen"), ("Shounen-ai", "Shounen-ai"), ("Slice of Life", "Slice of Life"),
+    ("Space", "Space"), ("Sports", "Sports"), ("Super Power", "Super Power"), ("Supernatural", "Supernatural"),
+    ("Tragedy", "Tragedy"), ("Vampire", "Vampire"), ("Yaoi", "Yaoi"), ("Yuri", "Yuri"),
+)
+
+class Manga(models.Model):
+    manga_name = models.CharField(max_length=1000)
+    manga_author = models.CharField(max_length=1000)
+    manga_illustrator = models.CharField(max_length=1000)
+    release_date = models.DateField('date published')
+    manga_genre = MultiSelectField(choices=genres, max_choices=1, default=None)
+    manga_score = models.FloatField(default=0.0)
+    manga_description = models.TextField(max_length=1000)
+    is_available = models.BooleanField(verbose_name="Is Available For Readers", default=True)
+
+
+
+
+
+
+
+
+
+
